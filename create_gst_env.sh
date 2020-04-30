4b171cc48c0837dab7d16e508db63aa29e16be61
Ling Wang@g731gw <LingWangNeuralEng@gmail.com>
2020-04-30 13:46:56 +0200

    create gst_env automatically

---
+++ create_gst_env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+./gst_check_env.sh
+
+gst_env_path="${GST_INSTALL_PATH}/bin/gst_env"
+py3_site="$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"].lstrip("/usr"))')"
+
+echo "export LD_LIBRARY_PATH=\"${GST_INSTALL_PATH}/${LIB_DIR}:\${LD_LIBRARY_PATH}\"" > ${gst_env_path}
+echo "export PATH=\"${GST_INSTALL_PATH}/bin:\$PATH\""  >> ${gst_env_path}
+echo "export PYTHONPATH=\"${GST_INSTALL_PATH}/${py3_site}:\$PYTHONPATH\"" >> ${gst_env_path}
+
+echo "before run any gst command use 'source ${gst_env_path}'"
--- gst_check_env.sh
+++ gst_check_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-var_names="GST_SRC_ROOT GST_BUILD_ROOT GST_REPO_NAMES GST_INSTALL_PATH"
+var_names="GST_SRC_ROOT GST_BUILD_ROOT GST_REPO_NAMES GST_INSTALL_PATH LIB_DIR"
 for var in ${var_names}
 do
     if [ -z "${!var}" ]
