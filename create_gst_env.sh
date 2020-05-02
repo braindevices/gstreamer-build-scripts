e060e91a8284531254abe08002cab8909d870f84
Ling Wang@g731gw <LingWangNeuralEng@gmail.com>
2020-05-02 11:50:39 +0200

    also export the GST_PLUGIN_SCANNER

--- create_gst_env.sh
+++ create_gst_env.sh
@@ -9,5 +9,5 @@ py3_site="$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"]
 echo "export LD_LIBRARY_PATH=\"${GST_INSTALL_PATH}/${LIB_DIR}:\${LD_LIBRARY_PATH}\"" > ${gst_env_path}
 echo "export PATH=\"${GST_INSTALL_PATH}/bin:\$PATH\""  >> ${gst_env_path}
 echo "export PYTHONPATH=\"${GST_INSTALL_PATH}/${py3_site}:\$PYTHONPATH\"" >> ${gst_env_path}
-
+echo "export GST_PLUGIN_SCANNER=\"${GST_INSTALL_PATH}/libexec/gstreamer-1.0/gst-plugin-scanner\"" >> ${gst_env_path}
 echo "before run any gst command use 'source ${gst_env_path}'"
