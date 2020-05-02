2d3502a01b54eb89c57ce33fe1bff834a8962e26
Ling Wang@g731gw <LingWangNeuralEng@gmail.com>
2020-05-02 13:53:56 +0200

    add comment

--- gst_build_env.sh
+++ gst_build_env.sh
@@ -1,3 +1,5 @@
+# source this file before build
+#this file is different per site, so do NOT commit to master branch
 export GST_SRC_ROOT="${HOME}/Projects/gstreamer/repos"
 export GST_BUILD_ROOT="${HOME}/Projects/gstreamer/builds"
 export GST_REPO_NAMES="gstreamer gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav gst-rtsp-server"
